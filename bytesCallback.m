dc058da98c281e1d2f7e9c87ff2ae26834b7fff3
nsaitarun-git <saitarun.nadipineni@gmail.com>
2025-07-10 12:10:55 +0100

    Updates

--- bytesCallback.m
+++ bytesCallback.m
@@ -1,27 +1,15 @@
 function bytesCallback(s,evt,expectedID,flag,offsets)
-<<<<<<< HEAD
+
 DF = 50;    % force scale
 DT = 1000;  % torque scale
-
 response = read(s,19,"uint8"); % Read 19 bytes of data
-=======
-
-response = read(s,19,"uint8"); % Read 19 bytes of data
-DF = 50;    % force scale
-DT = 1000;  % torque scale
->>>>>>> 762e46dd1eb658fc419dbb7a7c0103a68b0b061a
 
 if response(1)==0x55
     data = response(2:17);
     checksum = response(18);
     eop = response(19);
-<<<<<<< HEAD
 
     if eop==0xAA && data(1)==expectedID
-=======
-    if eop==0xAA && data(1)==expectedID
-
->>>>>>> 762e46dd1eb658fc419dbb7a7c0103a68b0b061a
         response = data;
 
         % Combine high and low bytes, then cast to int16
@@ -38,17 +26,11 @@ if response(1)==0x55
         end
 
         % Save data
-<<<<<<< HEAD
         s.Tag = '1';
         buffer = s.UserData;
         buffer(end+1,:) = [datevec(evt.AbsTime),Fx,Fy,Fz,Tx,Ty,Tz];
         s.UserData = buffer;
         s.Tag = '0';
-=======
-        buffer = s.UserData;
-        buffer(end+1,:) = [datevec(evt.AbsTime),Fx,Fy,Fz,Tx,Ty,Tz];
-        s.UserData = buffer;
->>>>>>> 762e46dd1eb658fc419dbb7a7c0103a68b0b061a
     end
 end
 end
